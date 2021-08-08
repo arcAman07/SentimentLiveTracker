e54bc05c68b6fb318463b90771c16e0f06c61bd8
Tanmay000009 <tanmayvyas09@gmail.com>
2021-08-09 01:05:27 +0530

    Basic fastapi structure added

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
+import uvicorn
+from fastapi import FastAPI
 
-from tweet_generator import tweet_generator
-TPCK = "xVkTAd1wDA3ySd2GY2TWARaEi"
-TSCK = "yrKhWbwj9P3s2aUgl8ho4b4TKdcCXYk7z1PDTR6jqvuusXzN4k"
-TPAK = "1299392071468425216-LUkjtSHB9cVXnlkAebcgoLIreWWNme"
-TSAK = "u5h73AaFqJP1X92BohYsNEAeg2ZRGw0ofFbgvKxiP47it"
-twitter_bot = tweet_generator.PersonTweeter('@TanmaysuchAnoob',TPCK,TSCK,TPAK,TSAK)
-random_tweet = twitter_bot.generate_random_tweet()
-print(random_tweet)
+app = FastAPI()
+
+@app.get('/')
+def index():
+    return {'message': 'Hello, stranger'}
+
+
+@app.get('/{name}')
+def get_name(name: str):
+    return {'message': f'Hello, {name}'}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
