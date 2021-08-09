bfe719074649caa270266efe91e7d5586505c4d6
Tanmay000009 <tanmayvyas09@gmail.com>
2021-08-09 13:55:55 +0530

    Model configured

--- app.py
+++ app.py
@@ -1,25 +1,45 @@
+from tensorflow import keras
 import uvicorn
 from fastapi import FastAPI
 from config import settings
 from tweet_generator import tweet_generator
-
+import tensorflow
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 app = FastAPI()
 
-@app.get('/')
+model = keras.models.load_model("TextSentiment_Analysis")
+
+tokenizer = Tokenizer(num_words=10000, oov_token="<OOV>")
+
+
+@ app.get('/')
 def index():
     return {'message': 'Hello, stranger'}
 
 
-@app.get('/{name}')
+@ app.get('/generate-tweet/{name}')
 def get_name(name: str):
     TPCK = settings.TPCK
     TSCK = settings.TSCK
     TPAK = settings.TPAK
     TSAK = settings.TSAK
-    twitter_bot = tweet_generator.PersonTweeter(name,TPCK,TSCK,TPAK,TSAK)
+    twitter_bot = tweet_generator.PersonTweeter(name, TPCK, TSCK, TPAK, TSAK)
     random_tweet = twitter_bot.generate_random_tweet()
     random_tweet = random_tweet
     return {'Tweet': random_tweet}
+
+
+@ app.get('/predict-sentiment/{str}')
+def get_name(str: str):
+    sentence = [str]
+    print(sentence)
+    seq = tokenizer.texts_to_sequences(sentence)
+    print(seq)
+    padded = pad_sequences(seq, maxlen=120,
+                           padding="post", truncating="post")
+    prediction = model.predict(padded)
+    return {'Predicted sentiment': prediction}
 
 
 if __name__ == '__main__':
